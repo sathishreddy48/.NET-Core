d92c6aefe456d7a1b8b8d0a35ada126026913486
sathi <sathi@DESKTOP-669CMP7>
2020-06-04 21:44:47 +0530

    intial project setup

---
+++ Core2/Core2/Controller/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace Core2.Controller
+{
+    public class HomeController : Microsoft.AspNetCore.Mvc.Controller
+    {
+        public string Index()
+        {
+            return "String data";
+        }
+    }
+}
---
+++ Core2/Core2/Startup.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace Core2
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            // this will internally calls MVCCore -- AddMvccore() method 
+            // so instead of writing services.AddMvcCore();  we can write above code
+
+
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+           
+            app.UseRouting();
+            app.UseStaticFiles();
+
+
+            app.UseEndpoints(endpoints =>
+            {
+                 // For default routing we need to add below lines
+                endpoints.MapControllerRoute(
+                       name: "default",
+                       pattern: "{controller=Home}/{action=Index}/{id?}");
+
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}
