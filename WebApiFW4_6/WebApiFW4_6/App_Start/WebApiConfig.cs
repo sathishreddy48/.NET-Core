7cd3a6419937565bc411b17bedb3d2276fc20b2e
sathi <sathi@DESKTOP-669CMP7>
2020-06-06 12:59:37 +0530

    Change default formmaters code

--- WebApiFW4_6/WebApiFW4_6/App_Start/WebApiConfig.cs
+++ WebApiFW4_6/WebApiFW4_6/App_Start/WebApiConfig.cs
@@ -19,6 +19,9 @@ namespace WebApiFW4_6
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Newtonsoft.Json.Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+
         }
     }
 }
