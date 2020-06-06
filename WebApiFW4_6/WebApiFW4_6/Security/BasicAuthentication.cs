8daea200462393c223ab936dbe66501f52d0f7b9
sathi <sathi@DESKTOP-669CMP7>
2020-06-06 16:00:59 +0530

    basic authentication incompleted , take it and finish the code

---
+++ WebApiFW4_6/WebApiFW4_6/Security/BasicAuthentication.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Security.Principal;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http.Controllers;
+
+namespace WebApiFW4_6.Security
+{
+    public class BasicAuthentication : System.Web.Http.Filters.AuthorizationFilterAttribute
+    {
+        public override void OnAuthorization(HttpActionContext actionContext)
+        {
+           // base.OnAuthorization(actionContext);
+           if(actionContext.Request.Headers.Authorization==null)
+           {
+                actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+           }
+           else
+           {
+                string authenticationtoken = actionContext.Request.Headers.Authorization.Parameter;
+                //base64encode username:password
+                string decodedAuthToken= Encoding.UTF8.GetString(Convert.FromBase64String(authenticationtoken));
+                string[] username_password = decodedAuthToken.Split(':');
+                string username = username_password[0];
+                string password = username_password[1];
+                if(Security.Login(username, password))
+                {
+                    Thread.CurrentPrincipal = new GenericPrincipal(new GenericIdentity(username), null);
+                }
+                else
+                {
+                    actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+                }
+           }
+        }
+    }
+}
---
+++ WebApiFW4_6/WebApiFW4_6/Security/Security.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiFW4_6.Security
+{
+    public class Security
+    {
+     public static bool Login(string username,string password)
+     {
+
+
+            return true;
+     }
+    }
+}
