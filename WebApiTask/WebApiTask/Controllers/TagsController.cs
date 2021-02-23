f27162534a70b9e940c1c149ee1e09df7509047f
sathi <sathi@DESKTOP-669CMP7>
2021-02-23 13:47:19 -0800

    changes

---
+++ WebApiTask/WebApiTask/Controllers/TagsController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApiTask.Repository.IRepository;
+
+namespace WebApiTask.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TagsController : ControllerBase
+    {
+
+        private IUnitOfWork unitOfWork;
+
+        public TagsController(IUnitOfWork unit)
+        {
+            unitOfWork = unit;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllAsync()
+        {
+              var result=  unitOfWork.Tags.GetAll();
+            return Ok(result);
+        }
+    }
+}
